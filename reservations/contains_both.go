867a77faf6e3155e117b42dc8c47f4f3d74e424c
LukasNiedling <lukasniedling2006@gmail.com>
2025-11-11 09:53:58 +0100

    test

--- reservations/contains_both.go
+++ reservations/contains_both.go
@@ -3,6 +3,19 @@ package reservations
 // ContainsBoth erwartet eine Liste von Strings und zwei Strings s1 und s2.
 // Liefert true, falls sowohl s1 als auch s2 in der Liste enthalten sind, sonst false.
 func ContainsBoth(list []string, s1 string, s2 string) bool {
-	
+	foundS1 := false
+	foundS2 := false
+
+	for i := 0; i < len(list); i++ {
+		if list[i] == s1 {
+			foundS1 = true
+		}
+		if list[i] == s2 {
+			foundS2 = true
+		}
+		if foundS1 && foundS2 {
+			return true
+		}
+	}
 	return false
 }
--- reservations/contains_in_order.go
+++ reservations/contains_in_order.go
@@ -3,6 +3,24 @@ package reservations
 // ContainsInOrder erwartet eine Liste von Strings und zwei Strings s1 und s2.
 // Liefert true, falls sowohl s1 als auch s2 in der Liste enthalten sind, und s1 vor s2 kommt.
 func ContainsInOrder(list []string, s1 string, s2 string) bool {
-	// TODO
+	foundS1 := false
+	foundS2 := false
+	var posA int
+	var posB int
+
+	for i := 0; i < len(list); i++ {
+		if list[i] == s1 {
+			foundS1 = true
+			posA = i
+		}
+		if list[i] == s2 {
+			foundS2 = true
+			posB = i
+		}
+	
+		if foundS1 && foundS2 && posA < posB {
+			return true
+		}
+	}
 	return false
 }
